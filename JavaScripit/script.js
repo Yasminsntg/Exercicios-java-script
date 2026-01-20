b57afda88d007f1f961176c044289ad94d418fa6
Yasmisntg <yasminma952@gmail.com>
2026-01-20 19:18:51 -0300

    feat: adiciona primeira parte do exercicio de JavaScipt

---
+++ JavaScripit/script.js
@@ -0,0 +1,60 @@
+// ATIVIDADE 1 
+
+// let num1 = 2;
+// let num2 = 10;
+
+// console.log("Soma: " +(num1+num2));
+// console.log("Subtração: " +(num1 - num2));
+// console.log("Multiplicação: " +(num1 * num2));
+// console.log("Divisão: " +(num1 / num2));
+
+// ATIVIDADE 2
+
+// let base = 6;
+// let altura = 3;
+
+// console.log ("multiplicação: " +(base * altura));
+
+
+// let base = 9;
+// let altura = 2;
+
+// console.log ("multiplicação:" +(base * altura));
+
+
+// ATIVIDADE 3
+
+// let real = 85.0;
+// let dolar = 5.0;
+
+// console.log ("divisao:" +(real / dolar))
+
+
+
+// ATIVIDADE 4
+
+// let base = 5;
+// let expoente = 3;
+
+// console.log ("potenciacao: " +(base ** expoente));
+
+// ATIVIDADE 5
+
+// let nota1 = 1.4;
+// let nota2 = 2.3;
+// let nota3 = 3.2;
+// let nota4 = 4.9;
+
+// let soma = nota1 + nota2+ nota3+ nota4;
+// let media = soma / 4;
+
+// console.log ("A media é: " +(media))
+
+// ATIVIDADE 6 
+
+// let c = 20; 
+// let f = (c * 9/5) +32; 
+
+// console.log (f);
+
+// ATIVIDADE 7 
