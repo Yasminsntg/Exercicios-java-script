4529b4a8554b8782b44669b8ad9845ac9362d2c7
Yasmisntg <yasminma952@gmail.com>
2026-03-02 21:02:23 -0300

    feat:adiciona pasta com novo assunto.

---
+++ assunto-novo.js
@@ -0,0 +1,15 @@
+// alert("hello world!");
+
+// let ok = confirm("deseja continuar?");
+
+// if (ok) {
+//     alert ("bem vindo!");
+// }else{
+//     alert("saindo do sistema");
+// }
+
+let base = 30;
+let altura = 6;
+let area = base * altura;
+
+console.log(`a área do triangulo é: ${área}`);
---
+++ atividade-usando-for.js
@@ -0,0 +1,131 @@
+// questão 1
+// for (let i = 1; i <= 20; i++) {
+//    console.log(`i: ${i}`);
+// }
+
+// // questão 2
+// for (let i = 10; i > 0; i--){
+//     console.log(`${i}`);
+// }
+
+// questão 3
+
+// questão 4
+// for (let i =1; <= 10; i++) {
+//     console.log(`7 x ${i} = ${7} `)
+// }
+
+// questão 5
+
+// questão 6
+// let Js = "javascript";
+// for(let i = 0; i < Js.length; i++) {
+//     console.log(Js [i]);
+// }
+
+// // questão 7
+// for (let i = 0; i < 50; i += 5) {
+//     console.log (i); formula para dizer os multiplos de um numero
+// }
+
+// for (let i = 0; i < 20; i += 2) {
+//     console.log (i); multiplos de 2
+// }
+
+// questão 8
+// let soma = 0;
+// for ( let i = 1; i <= 20; i++ ) {
+//     if ( i % 2 == 1) continue;
+
+//     soma += i;
+// }
+// console.log(soma);
+
+// questão 9
+// let expoente = 0;
+// for (let i = 1; i <= 10; i++) {
+//     expoente = i ** 2;
+//     console.log(expoente);
+// }
+
+// questão 10
+// let linhas = "";
+//  for (let i = 0; i < 10; i++) {
+//     linhas += "**";
+//  }
+//     console.log (linhas);
+
+// questão 11
+// let contador = 0;
+// while(contador < 10){
+//     console.log (contador);
+//     contador ++;
+// }
+
+// questão 12
+// let numero = 10;
+// while (cronometro > 0){
+//     console.log (cronometro);
+//     break;
+// }
+
+// questão 13
+// let contador = 10;
+// while (contador >= 0){
+//     console.log(contador);
+//     contador --;
+// }
+
+// console.log("lançar!");
+// }
+
+// questão 14
+// let soma = 0;
+// let contador =0;
+// while (contador < 5){
+//     let numero =
+// }
+
+// questão 15
+// let numero = 51;
+// while (numero % 7 !== ()){
+//     numero++;
+// }
+
+// console.log ("primeiro multiplo de 7 maior que 50")
+
+// questao 16
+
+// questão 17
+// let soma = 0;
+// let numero = 1;
+// while (soma <= 50){
+//     soma += numero;
+//     numero ++;
+// }
+
+// console.log("soma final:", soma);
+
+// questao 18
+// let numero = 1;
+// while (numero <= 1000){
+//     console.log(numero);
+//     numero*= 2;
+// }
+
+// console.log (numero);
+
+// questao 19
+// let altura = 1;
+// while (altura < 15){
+//     console.log ("altura", altura, "cm");
+//     altura += 2;
+// }
+
+// console.log ("altura final", altura, "cm");
+
+// questao 20
+// let condicao = false;
+// while (condicao){
+// console.log("Olá mundo");
+// }
