198a8c4c3eb111b227e133e5752e3bf4626cb467
Yasmisntg <yasminma952@gmail.com>
2026-03-03 21:01:47 -0300

    feat: adiciona questao seis.

--- assunto-novo.js
+++ assunto-novo.js
@@ -8,8 +8,64 @@
 //     alert("saindo do sistema");
 // }
 
-let base = 30;
-let altura = 6;
-let area = base * altura;
+// questao 1
 
-console.log(`a área do triangulo é: ${área}`);
+// let base = prompt("infome o valor da base:");
+// let altura = prompt("informe o valor da altura:");
+// let result = 0;
+
+// result = base * altura;
+
+// alert (`a are desse retângulo é ${result}cm`);
+
+// questao 2
+
+// let nota1 = parseFloat(prompt("Digite a nota 1"));
+// let nota2 = parseFloat(prompt("Digite a nota 2"));
+// let nota3 = parseFloat(prompt("Digite a nota 3"));
+// let nota4 = parseFloat(prompt("Digite a nota 4"));
+// let mediaaritmética =(nota1 + nota2 + nota3 + nota4) / 4;
+
+// alert (`a média é: ${mediaaritmética}`);
+
+// questao 3
+
+// let peso = parseFloat(prompt(`Digite o peso:`));
+// let altura = parseFloat(prompt(`Digite a altura:`));
+// let result = 0;
+
+// result = (peso / (altura ** 2));
+
+// alert (`o peso IMC é: ${result}`);
+
+// questao 4
+
+// let numero = parseFloat(prompt("didite um número:"));
+// if (numero <= 0){
+//     alert("o número é menor ou igual a zero");
+// } else{
+//     alert("o número é maior ou igual a zero");
+// }
+
+// questao 5
+
+// let saldo = parseFloat(prompt("Digite o valor do saldo: "));
+// let saque = parseFloat(prompt("Digite o valor do saque: "));
+
+// if (saque >= saldo){
+// alert ("saldo disponivel para o saque. Você possui:" (saldo- saque));
+// } else{
+//     alert("saldo indisponível ou insuficiente para o saque");
+// }
+
+
+// questao 6
+
+// let usuario = (prompt("Digite o seu nome:"));
+// let senha = (prompt("Digite a senha:"));
+
+// if (usuario === "admin" && senha === "1234"){
+//     alert ("Login autorizado!");
+// } else{
+//     alert ("usário ou senha inválido.");
+// }
